ccea52d50810c1b988b5f7b0e071145b4f371b12
tomchristopherperez <2470196889@qq.com>
2020-04-09 00:26:35 +0800

    Initialize

---
+++ PeerProject/RegExp.h
@@ -0,0 +1,14 @@
+﻿//
+// RegExp.h
+//
+// Abstracted std::tr1:regex Regular Expressions
+
+#pragma once
+
+
+namespace RegExp {
+
+    BOOL	Match(LPCTSTR szRegExp, LPCTSTR szContent);
+    size_t	Split(LPCTSTR szRegExp, LPCTSTR szContent, LPTSTR *pszResult);
+
+};
---
+++ PeerProject/Revision.h
@@ -0,0 +1,7 @@
+﻿//
+// No SVN Revision Data
+
+//#pragma once
+
+//#define __REVISION__
+//#define __MODAUTHOR__
